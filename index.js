58b6a6864ed9c242f0bcbbf46bd1fffbc9d3a2fd
mohit01717 <mohitgagneja943@gmail.com>
2024-04-14 10:34:59 +0530

    updaeted code

--- index.js
+++ index.js
@@ -2,10 +2,16 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const cors = require('cors');
+const { users } = require('./users');
+
 app.use(cors());
+
+
 app.get('/', (req, res) => {
-  res.sendFile(path.resolve('users.json'));
+  res.json(users);
 }); 
+
+
 
 app.listen(3000,()=>{
     console.log('Server is running on port 3000');
